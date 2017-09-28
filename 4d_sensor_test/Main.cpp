f7a0fe36456b4b65f6ca1752e279a8666f358548
Stephen Hara <sthara216@gmail.com>
2017-09-27 22:32:53 -0400

    add solution to question 2-2

--- 4d_sensor_test/Main.cpp
+++ 4d_sensor_test/Main.cpp
@@ -22,7 +22,6 @@ void questionOne() {
 
 void questionTwoOne(Image originalImage, Image& processImage, size_t columns, size_t rows) {
 	ColorHSL pixel;
-	int change = 0, noChange = 0;
 	for (int i = 0; i < columns; i++) {
 		for (int j = 0; j < rows; j++) {
 			pixel = originalImage.Image::pixelColor(i, j);
@@ -30,15 +29,9 @@ void questionTwoOne(Image originalImage, Image& processImage, size_t columns, si
 			if (isNoisy(pBrightness, surroundingBrightness)) {
 				pixel.lightness(surroundingBrightness);
 				processImage.Image::pixelColor(i, j, pixel);
-				change++;
-			}
-			else {
-				noChange++;
 			}
 		}
 	}
-	cout << "change pixels: " << change << endl;
-	cout << "no change pixels: " << noChange;
 
 	processImage.write("denoised_1.png");
 }
@@ -46,9 +39,15 @@ void questionTwoOne(Image originalImage, Image& processImage, size_t columns, si
 void questionTwoTwo(Image originalImage, Image& processImage, size_t columns, size_t rows) {
 	ColorHSL pixel;
 
-	for (int i = 0; i < columns; i++) {
-		for (int j = 0; j < rows; j++) {
+	for (int i = 1; i < columns - 1; i++) {
+		for (int j = 1; j < rows - 1; j++) {
 			pixel = originalImage.Image::pixelColor(i, j);
+			double pBrightness = brightness(pixel);
+			ComplexPixelBrightness cpb = surroundingPixelBrightnessAll(originalImage, columns, rows, i, j);
+
+			if (isNoisy(pBrightness, cpb.avgMiddleBrightness)) {
+				overwriteExtremePixels(originalImage, processImage, cpb.extremePixels, columns, rows, i, j, cpb.avgMiddleBrightness);
+			}
 		}
 	}
 
@@ -62,7 +61,7 @@ void questionTwo() {
 	size_t columns = original_image.Image::columns();
 	size_t rows = original_image.Image::rows();
 
-	questionTwoOne(original_image, processed_image, columns, rows);
+	//questionTwoOne(original_image, processed_image, columns, rows);
 	questionTwoTwo(original_image, processed_image_two, columns, rows);
 	cout << "Question 2 complete!" << endl;
 }
--- 4d_sensor_test/PixelAnalysis.h
+++ 4d_sensor_test/PixelAnalysis.h
@@ -3,13 +3,32 @@
 #include <Magick++.h>
 #include <iostream>
 #include <cstdlib>
+#include <vector>
+#include <exception>
+#include <numeric>
+#include <algorithm>
 
 using namespace Magick;
 
 int PIXEL_NOISE_THRESHOLD = 10;
 
+class InvalidPixelException : public std::exception {
+	virtual const char* msg() const throw(){
+		return "Attempted to go out of image bounds for this calculation.";
+	}
+} InvalidPixelException;
+
+struct ComplexPixelBrightness {
+	double avgMiddleBrightness;
+	std::vector<ColorHSL> extremePixels;
+};
+
 double brightness(ColorHSL pixel) {
 	return pixel.lightness();
+}
+
+double addPixelBrightness(double a, ColorHSL b) {
+	return a + b.lightness();
 }
 
 /**
@@ -41,9 +60,89 @@ double surroundingPixelBrightnessCardinal(Image srcImage, size_t maxCols, size_t
 }
 
 /**
+	Calculate the brightness of the pixels surrounding the target pixel (provided by (col, row))
+	@return a ComplexPixelBrightness reference with the 9 pixels split into extreme brightness
+	pixels, middle brightness pixels, and the average brightness of the middle pixels.
+*/
+ComplexPixelBrightness surroundingPixelBrightnessAll(Image srcImage, size_t maxCols, size_t maxRows, int col, int row) {
+	if (col == 0 || col == maxCols || row == 0 || row == maxRows) {
+		throw InvalidPixelException;
+	}
+	ComplexPixelBrightness cpb;
+	std::vector<ColorHSL> allPixels;
+
+	for (int c = col - 1; c <= col + 1; c++) {
+		for (int r = row - 1; r <= row + 1; r++) {
+			allPixels.push_back((ColorHSL)srcImage.pixelColor(c, r));
+		}
+	}
+
+	double brightnessSum = std::accumulate(allPixels.begin(), allPixels.end(), 0, addPixelBrightness);
+
+	double bSum = 0;
+	for (auto i = allPixels.begin(); i < allPixels.end(); i++) {
+		bSum += i->lightness();
+	}
+
+	double avgBrightness = brightnessSum / allPixels.size();
+
+	std::sort(allPixels.begin(), allPixels.end(), [](ColorHSL a, ColorHSL b) -> bool {
+		return b.lightness() > a.lightness();
+	});
+
+	std::vector<ColorHSL> extremePixels;
+	
+	// get the front two brightnesses (the two minimums)
+	ColorHSL tempBrightness = allPixels.front();
+	extremePixels.push_back(tempBrightness);
+	allPixels.erase(allPixels.begin());
+
+	tempBrightness = allPixels.front();
+	extremePixels.push_back(tempBrightness);
+	allPixels.erase(allPixels.begin());
+
+	// now get the top two (two maximums)
+	tempBrightness = allPixels.back();
+	extremePixels.push_back(tempBrightness);
+	allPixels.pop_back();
+
+	tempBrightness = allPixels.back();
+	extremePixels.push_back(tempBrightness);
+	allPixels.pop_back();
+
+	// construct our return object
+	cpb.extremePixels = extremePixels;
+	cpb.avgMiddleBrightness = std::accumulate(allPixels.begin(), allPixels.end(), 0.0, addPixelBrightness) / allPixels.size();
+
+	bSum = 0;
+	for (auto i = allPixels.begin(); i < allPixels.end(); i++) {
+		bSum += i->lightness();
+	}
+
+
+	return cpb;
+}
+
+/**
 	Return true if the pixel brightness and surrounding brightness exceed the threshold to be
 	classified as noisy.
 */
 bool isNoisy(double pixelBrightness, double surroundingBrightness) {
 	return 255 * abs(pixelBrightness - surroundingBrightness) > PIXEL_NOISE_THRESHOLD;
+}
+
+void overwriteExtremePixels(Image srcImage, Image& targetImage, std::vector<ColorHSL> overwriteList, size_t maxCols, size_t maxRows, int col, int row, double targetBrightness) {
+	ColorHSL pixel;
+
+	for (int c = col - 1; c <= col + 1; c++) {
+		for (int r = row - 1; r <= row + 1; r++) {
+			pixel = srcImage.Image::pixelColor(c, r);
+			if (std::any_of(overwriteList.begin(), overwriteList.end(), [pixel](ColorHSL a) { return a.lightness() == pixel.lightness(); })) {
+				pixel.lightness(targetBrightness);
+				targetImage.Image::pixelColor(c, r, pixel);
+			}
+		}
+	}
+
+	return;
 }
